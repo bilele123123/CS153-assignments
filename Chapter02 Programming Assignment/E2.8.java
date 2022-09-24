c18fe5c06525524e13e2465546063114a9b4ee0c
bilele123123 <69129294+bilele123123@users.noreply.github.com>
2022-09-24 14:43:20 -0700

    Add files via upload
    display an empty java frame

---
+++ Chapter02 Programming Assignment/E2.8.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+class BrighterDemo{ 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(500, 600);
+        frame.setTitle("An Alien Face");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+  
+      }
+}
